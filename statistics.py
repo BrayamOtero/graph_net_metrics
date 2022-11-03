9bff16c8eeb808a5355d8bda8e0cba74d9e99cd4
Brayam Otero <davidotero@unicauca.edu.co>
2022-11-02 22:20:25 -0500

    feat: remuse feature

--- statistics.py
+++ statistics.py
@@ -31,6 +31,8 @@ def getMean(df, metric):
     return np.mean(df.loc[:, metric])
 
 def meanData(meanData, newData):
+    if meanData == 0:
+        return newData
     meanData += newData
     meanData /= 2
     return meanData
@@ -97,6 +99,23 @@ def getMetricXHour(metric_historic):
         since_index = to_index    
     return metricsXhour
 
+'''
+Obtiene el nombre del agente desde el nombre del csv metric_[name_agent].csv
+'''
+def getNameAgent(name_raw):
+    m = re.search(r'metrics_([a-zA-Z]+)', name_raw)
+    if m:
+        return m.group(1)
+    else: 
+        return False
+
+def find_number(string):
+    match = re.findall(r'\d+', string)
+    if match:
+        return match[0]
+    else:
+        return 0
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:        
         raise Exception('Es necesario colocar un argumento')        
@@ -105,6 +124,8 @@ if __name__ == "__main__":
     name_agent = "default"
     #graficar todos los datos de los agentes DRL
     plot_all = False
+
+    resume_metrics = False
 
     for arg in sys.argv[1:]:
         option = arg.split("=")
@@ -116,6 +137,8 @@ if __name__ == "__main__":
             name_agent = option[1]
         if option[0] == '--plot_all':
             plot_all = True
+        if option[0] == '--resume':
+            resume_metrics = True
 
     if plot_all:
         list_name = mFile.get_files_info_net()
@@ -145,6 +168,50 @@ if __name__ == "__main__":
             plt.show()
         exit()
 
+    if resume_metrics:
+        list_name = mFile.get_files_info_net()        
+        info_metrcis = {}
+        for name_info in list_name:
+            dir_file = "./metrics_csv/" + name_info
+            # print(dir_file)
+            df = pd.read_csv(dir_file)            
+            info_metrcis[name_info] = df
+                
+        metrics = ["th" ,"delay", "loss", "qlen"]
+        data = {}
+        # dict_hour = {"00:00" : [], "01:00" : [],"02:00" : [],"03:00" : [],"04:00" : [],"05:00" : [],"06:00" : [],"07:00" : [],"08:00" : [],"09:00" : [],"10:00" : [],"11:00" : [],"12:00" : [],"13:00" : [],"14:00" : [],"15:00" : [],"16:00" : [],"17:00" : [],"18:00" : [],"19:00" : [],"20:00" : [],"21:00" : [],"22:00" : [],"23:00" : []}        
+        for raw_name_agent, df_info in info_metrcis.items():            
+            name = getNameAgent(raw_name_agent)                
+            if (name not in data):
+                data.setdefault(name, {"th" : [], "delay" : [], "loss" : [], "qlen" : []})
+            for metric in metrics:
+                if metric == metrics[0] and int(find_number(raw_name_agent)) < 6:
+                    continue
+                data[name][metric].append(df_info.loc[:, metric].tolist())
+        
+        ind = np.arange(24) #24 hours         
+        num_agent = len(data.keys())
+        width = 0.8/num_agent
+        for metric in metrics:
+            cout_move = -1
+            for nameAgent in data.keys():                
+                npInfo = np.array(data[nameAgent][metric])
+                if metric == metrics[0]:
+                    npInfo = npInfo/37
+                meanMetric = np.mean(npInfo, axis=0)
+                data[nameAgent][metric] = meanMetric.tolist()
+
+                plt.bar(ind + cout_move * width, data[nameAgent][metric], width, label=nameAgent)                
+                cout_move += 1
+
+            plt.ylabel(metric + ' mean')
+            plt.title('Result of ' + metric)
+
+            plt.xticks(ind + width/num_agent,  generateHours())
+            plt.legend(loc='best')
+            plt.show()
+        
+        exit()
     mFile.createDir('metrics_csv')
                     
     cvs_sorted = mFile.get_flies_sorted(PATH)
@@ -167,7 +234,7 @@ if __name__ == "__main__":
 
         delay_mean = getMean(df, "delay")
         loss_mean = getMean(df, "pkloss")
-        th_mean = getMean(df, "used_bw")/1000 # EL bw se escala a 10^3
+        th_mean = getMean(df, "free_bw")/37 # EL bw se escala a 10^3
         q1_mean = getMean(df, "qlen->")
         q2_mean = getMean(df, "<-qlen")
 
